76124188ab7955967ffd4f8c631f6f3488e3506e
Joel K George <joelk9895@gmail.com>
2024-03-29 17:04:31 +0530

    Almost final

--- app/pages/landing/sections/Hero/Components/SvgCircle.tsx
+++ app/pages/landing/sections/Hero/Components/SvgCircle.tsx
@@ -1,55 +1,57 @@
 export default function Circle() {
-  if (window.innerWidth > 720) {
-    return (
-      <svg
-        width="1250"
-        height="1250"
-        viewBox="0 0 1250 1250"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute -bottom-32 md:bottom-10 -z-10 w-[100vw]"
-      >
-        <circle cx="625" cy="400" r="700.5" stroke="#374151FF" />
-      </svg>
-    );
-  } else if (window.innerWidth > 430) {
-    return (
-      <svg
-        width="900"
-        height="900"
-        viewBox="0 0 900 900"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute bottom-10  -z-10 w-[100vw]"
-      >
-        <circle cx="450" cy="400" r="700.5" stroke="#374151FF" />
-      </svg>
-    );
-  } else if (window.innerWidth > 320) {
-    return (
-      <svg
-        width="1000"
-        height="1000"
-        viewBox="0 0 1000 1000"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute -bottom-32 md:bottom-10 -z-10 w-[100vw]"
-      >
-        <circle cx="500" cy="400" r="900.5" stroke="#374151FF" />
-      </svg>
-    );
-  } else {
-    return (
-      <svg
-        width="900"
-        height="900"
-        viewBox="0 0 900 900"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute bottom-10  -z-10 w-[100vw]"
-      >
-        <circle cx="450" cy="400" r="700.5" stroke="#374151FF" />
-      </svg>
-    );
+  if (typeof window != "undefined") {
+    if (window.innerWidth > 720) {
+      return (
+        <svg
+          width="1250"
+          height="1250"
+          viewBox="0 0 1250 1250"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute -bottom-32 md:bottom-10 -z-10 w-[100vw]"
+        >
+          <circle cx="625" cy="400" r="700.5" stroke="#374151FF" />
+        </svg>
+      );
+    } else if (window.innerWidth > 430) {
+      return (
+        <svg
+          width="900"
+          height="900"
+          viewBox="0 0 900 900"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute bottom-10  -z-10 w-[100vw]"
+        >
+          <circle cx="450" cy="400" r="700.5" stroke="#374151FF" />
+        </svg>
+      );
+    } else if (window.innerWidth > 320) {
+      return (
+        <svg
+          width="1000"
+          height="1000"
+          viewBox="0 0 1000 1000"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute -bottom-32 md:bottom-10 -z-10 w-[100vw]"
+        >
+          <circle cx="500" cy="400" r="900.5" stroke="#374151FF" />
+        </svg>
+      );
+    } else {
+      return (
+        <svg
+          width="900"
+          height="900"
+          viewBox="0 0 900 900"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute bottom-10  -z-10 w-[100vw]"
+        >
+          <circle cx="450" cy="400" r="700.5" stroke="#374151FF" />
+        </svg>
+      );
+    }
   }
 }
